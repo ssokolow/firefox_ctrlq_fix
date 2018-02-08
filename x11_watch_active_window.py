d6cccf521be9dbe0e420bd08cff7a289ca599d03
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2018-02-08 12:22:07 -0500

    Merge the pre-binding phase for existing windows and filter for Firefox
    (from the net_client_list.py POC)

--- x11_watch_active_window.py
+++ x11_watch_active_window.py
@@ -21,9 +21,11 @@ This listens for changes to it and then hides duplicate events
 so it only reacts to title changes once.
 """
 
+FIREFOX_WINCLASS = "Firefox"
+
 from contextlib import contextmanager
 import Xlib
-import Xlib.display
+from Xlib import X, display
 
 # Connect to the X server and get the root window
 disp = Xlib.display.Display()
@@ -31,6 +33,7 @@ root = disp.screen().root
 
 # Prepare the property names we use so they can be fed into X11 APIs
 NET_ACTIVE_WINDOW = disp.intern_atom('_NET_ACTIVE_WINDOW')
+NET_CLIENT_LIST = disp.intern_atom('_NET_CLIENT_LIST')
 
 last_seen = {'xid': None}
 
@@ -63,6 +66,10 @@ def grab_key(window):
     if not window:  # TODO: ...or already seen
         return  # Allow null windows here for robustness and simple structure
 
+    winclass = window.get_wm_class()
+    if not (winclass and winclass[-1] == FIREFOX_WINCLASS):
+        return  # Skip non-Firefox windows
+
     print("TODO: Bind Ctrl+Q for new window if Firefox")
 
 def handle_xevent(event):
@@ -84,7 +91,11 @@ if __name__ == '__main__':
     # Listen for _NET_ACTIVE_WINDOW changes
     root.change_attributes(event_mask=Xlib.X.PropertyChangeMask)
 
-    # TODO: Bind existing windows (eg. in case Firefox is already focused)
+    # Bind existing windows (eg. in case Firefox is already focused)
+    for xid in root.get_full_property(
+            NET_CLIENT_LIST, X.AnyPropertyType).value:
+        with window_obj(xid) as new_win:
+            grab_key(new_win)
 
     while True:  # next_event() sleeps until we get an event
         handle_xevent(disp.next_event())
